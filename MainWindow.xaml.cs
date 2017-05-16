3a75441dc37a2bf01ef1d549c2e5c226739f0310
Alper Ozaydin <aozaydi1@binghamton.edu>
2017-05-16 23:48:13 +0200

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DocumentManager _documentManager;
+
+        private readonly PrintManager _printManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _documentManager = new DocumentManager(body);
+            _printManager = new PrintManager(body);
+
+            //if (_documentManager.OpenDocument())
+            //    status.Text = "Document loaded.";
+        }
+
+        private void TextEditorToolbar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (toolbar.IsSynchronizing) return;
+            
+            ComboBox source = e.OriginalSource as ComboBox;
+            if (source == null) return;
+
+            switch (source.Name)
+            {
+                case "fontList":
+                    _documentManager.ApplyToSelection(TextBlock.FontFamilyProperty, source.SelectedItem);
+                    break;
+                case "fontSize":
+                    _documentManager.ApplyToSelection(TextBlock.FontSizeProperty, source.SelectedItem);
+                    break;
+            }
+            body.Focus();
+        }
+
+        private void Body_OnSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            toolbar.SynchronizeWith(body.Selection);
+        }
+
+        private void NewDocument(object sender, ExecutedRoutedEventArgs e)
+        {
+            _documentManager.NewDocument();
+            status.Text = "New Document";
+        }
+
+        private void OpenDocument(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_documentManager.OpenDocument())
+                status.Text = "Document loaded.";
+        }
+
+        private void SaveDocument(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_documentManager.SaveDocument())
+                status.Text = "Document saved.";
+        }
+
+        private void SaveDocumentAs(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_documentManager.SaveDocumentAs())
+                status.Text = "Document saved.";
+        }
+
+        private void ApplicationClose(object sender, ExecutedRoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SaveDocument_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _documentManager.CanSaveDocument();
+        }
+
+        private void PrintDocument(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_printManager.Print())
+                status.Text = "Document sent to printer.";
+        }
+
+        private void PrintPreview(object sender, ExecutedRoutedEventArgs e)
+        {
+            _printManager.PrintPreview();
+        }
+    }
+}
---
+++ TextEditorToolbar.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing.Text;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Interaction logic for TextEditorToolbar.xaml
+    /// </summary>
+    public partial class TextEditorToolbar : UserControl
+    {
+        public TextEditorToolbar()
+        {
+            Loaded += UserControl_Loaded;
+            InitializeComponent();
+
+        }
+
+        public bool IsSynchronizing { get; private set; }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (double i = 8; i < 48; i += 2)
+            {
+                fontSize.Items.Add(i);
+            }
+            var myFontList = new InstalledFontCollection().Families;
+
+            foreach (var f in myFontList)
+            {
+                fontList.Items.Add(f.Name);
+            }
+        }
+
+        public void SynchronizeWith(TextSelection selection)
+        {
+            IsSynchronizing = true;
+
+            Synchronize<FontStyle>(selection, TextBlock.FontStyleProperty, SetFontStyle);
+            Synchronize<double>(selection, TextBlock.FontSizeProperty, SetFontSize);
+            Synchronize<FontFamily>(selection, TextBlock.FontFamilyProperty, SetFontFamily);
+            Synchronize<FontWeight>(selection, TextBlock.FontWeightProperty, SetFontWeight);
+            Synchronize<TextDecorationCollection>(selection, TextBlock.TextDecorationsProperty, SetTextDecoration);
+            IsSynchronizing = false;
+        }
+
+        private void Synchronize<T>(TextSelection selection, DependencyProperty property, Action<T> methodToCall)
+        {
+            object value = selection.GetPropertyValue(property);
+            if (value != DependencyProperty.UnsetValue) methodToCall((T) value);
+        }
+
+        private void SetFontSize(double size)
+        {
+            fontSize.SelectedValue = size;
+        }
+
+        private void SetFontFamily(FontFamily family)
+        {
+            fontList.SelectedItem = family.ToString();
+        }
+
+        private void SetFontStyle(FontStyle style)
+        {
+            ItalicButton.IsChecked = style == FontStyles.Italic;
+        }
+
+        private void SetFontWeight(FontWeight weight)
+        {
+            BoldButton.IsChecked = weight == FontWeights.Bold;
+        }
+
+        private void SetTextDecoration(TextDecorationCollection decoration)
+        {
+           UnderlineButton.IsChecked = decoration == TextDecorations.Underline;
+        }
+    }
+}
